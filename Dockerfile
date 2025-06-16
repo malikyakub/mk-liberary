ae34dbe63f30291ffb694c279d4f54ee1055bb01
malik yakub <malikyakub@hotmail.com>
2025-06-16 19:55:33 +0300

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,27 @@
 FROM php:8.1-apache
 
-# Enable mysqli (used in your code)
+# Install system dependencies needed by Composer
+RUN apt-get update && apt-get install -y \
+    unzip \
+    zip \
+    git \
+    curl
+
+# Enable mysqli extension
 RUN docker-php-ext-install mysqli
 
-# Copy all project files into the container
-COPY . /var/www/html/
+# Install Composer globally
+RUN curl -sS https://getcomposer.org/installer | php && \
+    mv composer.phar /usr/local/bin/composer
 
-# Set working directory
+# Set working directory inside the container
 WORKDIR /var/www/html/
 
-# Expose port 80 (for HTTP)
+# Copy project files into container
+COPY . .
+
+# Install PHP dependencies from composer.json
+RUN composer install
+
+# Expose port 80 for Apache
 EXPOSE 80
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8a06b5c59aaa96c726090033d0bf21b49a989749',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8a06b5c59aaa96c726090033d0bf21b49a989749',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
