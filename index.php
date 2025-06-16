e3b8dd8c086e6bde173da0bc3924f7a3418b5fa0
malik yakub <malikyakub@hotmail.com>
2025-06-16 19:40:58 +0300

    modified index page

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
-<!-- mk-library/index.php -->
 <?php
-echo "Welcome to MK Library!";
-header("Location: login.html");
+header("Location: login.php");
 exit();
+?>
+<!-- This won't be shown if header works, but stays as fallback -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="0;url=login.php">
+</head>
+
+<body>If you're not redirected, <a href="login.php">click here</a>.</body>
+
+</html>
